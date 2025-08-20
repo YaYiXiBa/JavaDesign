369143a2467cef2eca6a1d7a50ea1df38a55f476
zxp <15630703781@163.com>
2025-08-20 18:09:23 +0800

    桥接模式

---
+++ src/main/java/com/xp/javadesign/brige/BaseCoffee.java
@@ -0,0 +1,32 @@
+package com.xp.javadesign.brige;
+
+public class BaseCoffee {
+    /**
+     * 杯型：小，中，大，超大
+     */
+    private String cupSize;
+    /**
+     * 咖啡豆：普通，精选
+     */
+    private String coffeeBean;
+    /**
+     * 温度：加冰，正常冰，少冰
+     */
+    private String temperature;
+    /**
+     * 名称
+     */
+    private String name;
+    /**
+     * 甜度：不另外加糖，少少甜，少甜，标准甜，微甜，五分，七分，三分等等。。。。
+     */
+    private String sugar;
+    /**
+     * 奶：不加，单倍，双倍
+     */
+    private String milk;
+    /**
+     * 咖啡浓度：默认，加份浓缩
+     */
+    private String Strength;
+}
